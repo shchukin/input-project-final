e0184937f6ef8d2059699b205c9326066ebcbc31
Anton Shchukin <a.a.shchukin@gmail.com>
2023-08-13 10:50:38 +0700

    select

--- scripts/_input.js
+++ scripts/_input.js
@@ -4,7 +4,6 @@
         $('.input').removeClass('input--error');
     });
 
-
     $('.input--expandable').each(function () {
         $(this).css('height', 'auto');
         $(this).css('height', $(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10) + 'px');
@@ -13,5 +12,12 @@
         $(this).css('height', $(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10) + 'px');
     });
 
+    $('select').on('change',function () {
+        if( $(this).val() === 'placeholder' ) {
+            $(this).addClass('input--placeholder-is-chosen');
+        } else {
+            $(this).removeClass('input--placeholder-is-chosen');
+        }
+    })
 
 })(jQuery);
